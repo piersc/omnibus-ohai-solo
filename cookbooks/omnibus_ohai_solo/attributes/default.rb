3552883830e881db163409725a4bc247e2e2a48a
Ryan Walker <ryan.walker@rackspace.com>
2013-12-13 15:39:49 -0600

    Allow timestamp to be controlled

--- cookbooks/omnibus_ohai_solo/attributes/default.rb
+++ cookbooks/omnibus_ohai_solo/attributes/default.rb
@@ -1,5 +1,6 @@
 default[:omnibus_ohai_solo][:repo] = "https://github.com/ryandub/omnibus-ohai-solo.git"
 default[:omnibus_ohai_solo][:ref]  = "master"
 default[:omnibus_ohai_solo][:release_environment] = "dev"
+default[:omnibus_ohai_solo][:append_timestamp] = "true"
 default[:ohai_plugins][:repo] = "https://github.com/rackerlabs/ohai-plugins.git"
 default[:ohai_plugins][:ref]  = "master"
